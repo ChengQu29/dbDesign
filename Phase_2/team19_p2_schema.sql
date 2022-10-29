d162f09a0ab3eb355b0a58559e98dc223d53b608
Khang Tran <khangtran@Khangs-MacBook-Pro.local>
2022-10-28 17:53:03 -0700

    replace type double undescore by single one

--- Phase_2/team19_p2_schema.sql
+++ Phase_2/team19_p2_schema.sql
@@ -40,11 +40,10 @@ CREATE TABLE Half (
 	sink INT NOT NULL,
 	commode INT NOT NULL,
 	bidet INT NOT NULL,
-	is_primary BOOLEAN NOT NULL,
 	name VARCHAR(240),
-	FK_Half_email__HouseHold_email VARCHAR(240) NOT NULL,
-	FOREIGN KEY (FK_Half_email__HouseHold_email) REFERENCES HouseHold(email),
-	PRIMARY KEY (number, FK_Half_email__HouseHold_email)
+	FK_Half_email_HouseHold_email VARCHAR(240) NOT NULL,
+	FOREIGN KEY (FK_Half_email_HouseHold_email) REFERENCES HouseHold(email),
+	PRIMARY KEY (number, FK_Half_email_HouseHold_email)
 );
 
 CREATE TABLE Full (
@@ -56,9 +55,9 @@ CREATE TABLE Full (
 	bathtub INT NOT NULL,
 	shower INT NOT NULL,
 	tub_shower INT NOT NULL,
-	FK_Full_email__HouseHold_email VARCHAR(240) NOT NULL,
-	FOREIGN KEY (FK_Full_email__HouseHold_email) REFERENCES HouseHold(email),
-	PRIMARY KEY (number, FK_Full_email__HouseHold_email)
+	FK_Full_email_HouseHold_email VARCHAR(240) NOT NULL,
+	FOREIGN KEY (FK_Full_email_HouseHold_email) REFERENCES HouseHold(email),
+	PRIMARY KEY (number, FK_Full_email_HouseHold_email)
 );
 
 -- Appliance Information
@@ -71,79 +70,79 @@ CREATE TABLE Manufacturer (
 
 CREATE TABLE Freezer (
 	freezer_id int NOT NULL AUTO_INCREMENT,
-	FK_Freezer_email__HouseHold_email varchar(240) NOT NULL,
+	FK_Freezer_email_HouseHold_email varchar(240) NOT NULL,
     model_name VARCHAR(240),
 	name VARCHAR(240) NOT NULL,
 	model_type VARCHAR(240) NOT NULL,   -- type -> Model_type because type means something in sql. Need to also change in other reports
-	PRIMARY KEY (freezer_id, FK_Freezer_email__HouseHold_email),
-	FOREIGN KEY (FK_Freezer_email__HouseHold_email) REFERENCES HouseHold(email),
+	PRIMARY KEY (freezer_id, FK_Freezer_email_HouseHold_email),
+	FOREIGN KEY (FK_Freezer_email_HouseHold_email) REFERENCES HouseHold(email),
 	FOREIGN KEY (name) REFERENCES Manufacturer(name)
 );
 
 CREATE TABLE Washer (
 	washer_id int NOT NULL AUTO_INCREMENT,
-	FK_Washer_email__HouseHold_email varchar(240) NOT NULL,
+	FK_Washer_email_HouseHold_email varchar(240) NOT NULL,
     model_name VARCHAR(240),
 	name VARCHAR(240) NOT NULL,
 	loading_type VARCHAR(240) NOT NULL,   -- type -> Model_type because type means something in sql. Need to also change in other reports
-	PRIMARY KEY (washer_id, FK_Washer_email__HouseHold_email),
-	FOREIGN KEY (FK_Washer_email__HouseHold_email) REFERENCES HouseHold(email),
+	PRIMARY KEY (washer_id, FK_Washer_email_HouseHold_email),
+	FOREIGN KEY (FK_Washer_email_HouseHold_email) REFERENCES HouseHold(email),
 	FOREIGN KEY (name) REFERENCES Manufacturer(name)
 );
 
 CREATE TABLE Dryer (
 	dryer_id int NOT NULL AUTO_INCREMENT,
-	FK_Dryer_email__HouseHold_email varchar(240) NOT NULL,
+	FK_Dryer_email_HouseHold_email varchar(240) NOT NULL,
     Model_name VARCHAR(240) NOT NULL,
 	name VARCHAR(240) NOT NULL,
 	heat_source VARCHAR(240) NOT NULL,   
-	PRIMARY KEY (dryer_id, FK_Dryer_email__HouseHold_email),
-	FOREIGN KEY (FK_Dryer_email__HouseHold_email) REFERENCES HouseHold(email),
+	PRIMARY KEY (dryer_id, FK_Dryer_email_HouseHold_email),
+	FOREIGN KEY (FK_Dryer_email_HouseHold_email) REFERENCES HouseHold(email),
 	FOREIGN KEY (name) REFERENCES Manufacturer(name)
 );
 
 CREATE TABLE TV (
     tv_id int NOT NULL AUTO_INCREMENT,
-    FK_tv_email__HouseHold_email varchar(240) NOT NULL,
+    FK_tv_email_HouseHold_email varchar(240) NOT NULL,
     model_name VARCHAR(240) NOT NULL,
 	name VARCHAR(240) NOT NULL,
 	display_type VARCHAR(240) NOT NULL,
 	display_size FLOAT(21, 11) NOT NULL,
 	maximum_resolution VARCHAR(240) NOT NULL,	
-	PRIMARY KEY (tv_id, FK_tv_email__HouseHold_email),
-	FOREIGN KEY (FK_tv_email__HouseHold_email) REFERENCES HouseHold(email),
+	PRIMARY KEY (tv_id, FK_tv_email_HouseHold_email),
+	FOREIGN KEY (FK_tv_email_HouseHold_email) REFERENCES HouseHold(email),
 	FOREIGN KEY (name) REFERENCES Manufacturer(name)
 );
 
 CREATE TABLE Cooker (
     cooker_id int NOT NULL AUTO_INCREMENT,
-    FK_cooker_email__HouseHold_email varchar(240) NOT NULL,
+    FK_cooker_email_HouseHold_email varchar(240) NOT NULL,
     model_name VARCHAR(240) NOT NULL,
     name VARCHAR(240) NOT NULL,
-    PRIMARY KEY (cooker_id, FK_cooker_email__HouseHold_email),
-	FOREIGN KEY (FK_cooker_email__HouseHold_email) REFERENCES HouseHold(email),
+    PRIMARY KEY (cooker_id, FK_cooker_email_HouseHold_email),
+	FOREIGN KEY (FK_cooker_email_HouseHold_email) REFERENCES HouseHold(email),
 	FOREIGN KEY (name) REFERENCES Manufacturer(name)
 
 );
 
 CREATE TABLE Oven (
     FK_Oven_id_Cooker_cooker_id int NOT NULL,
-    FK_oven_email__HouseHold_email varchar(240) NOT NULL,
+    FK_oven_email_HouseHold_email varchar(240) NOT NULL,
 	has_gas_heat_source BOOLEAN NOT NULL, 
 	has_electric_heat_source BOOLEAN NOT NULL,
 	has_microwave_heat_source BOOLEAN NOT NULL, 
 	oven_type VARCHAR(240) NOT NULL, 
-	PRIMARY KEY (FK_Oven_id_Cooker_cooker_id, FK_oven_email__HouseHold_email),
+	PRIMARY KEY (FK_Oven_id_Cooker_cooker_id, FK_oven_email_HouseHold_email),
 	FOREIGN KEY (FK_Oven_id_Cooker_cooker_id) REFERENCES Cooker(cooker_id),
-	FOREIGN KEY (FK_oven_email__HouseHold_email) REFERENCES HouseHold(email)
+	FOREIGN KEY (FK_oven_email_HouseHold_email) REFERENCES HouseHold(email)
 );
 
 CREATE TABLE Cooktop (
     FK_Cooktop_id_Cooker_cooker_id int NOT NULL,
-    FK_cooktop_email__HouseHold_email varchar(240) NOT NULL,
+    FK_cooktop_email_HouseHold_email varchar(240) NOT NULL,
 	heat_source VARCHAR(240) NOT NULL, 
-	PRIMARY KEY (FK_Cooktop_id_Cooker_cooker_id, FK_cooktop_email__HouseHold_email),
+	PRIMARY KEY (FK_Cooktop_id_Cooker_cooker_id, FK_cooktop_email_HouseHold_email),
 	FOREIGN KEY (FK_Cooktop_id_Cooker_cooker_id) REFERENCES Cooker(cooker_id),
-	FOREIGN KEY (FK_cooktop_email__HouseHold_email) REFERENCES HouseHold(email)
+	FOREIGN KEY (FK_cooktop_email_HouseHold_email) REFERENCES HouseHold(email)
 );
 
--- Phase_2/test.sql
+++ Phase_2/test.sql
@@ -9,10 +9,10 @@ INSERT INTO PhoneNumber (area_code, number, phone_type, FK_PhoneNumber_email_Hou
 VALUES ('905', '9224143', 'home', 'ktran322@gatech.edu');
 
 -- bathroom info
-INSERT INTO Half (number, sink, commode, bidet, is_primary, name, FK_Half_email__HouseHold_email)
+INSERT INTO Half (number, sink, commode, bidet, is_primary, name, FK_Half_email_HouseHold_email)
 VALUES (2, 1, 1, 1, False,  'dummyname', 'ktran322@gatech.edu');
 
-INSERT INTO Full (number, sink, commode, bidet, is_primary, bathtub, shower, tub_shower, FK_Full_email__HouseHold_email)
+INSERT INTO Full (number, sink, commode, bidet, is_primary, bathtub, shower, tub_shower, FK_Full_email_HouseHold_email)
 VALUES (1, 1, 1, 1, True,  0, 1, 0, 'ktran322@gatech.edu');
 
 
@@ -21,8 +21,8 @@ insert into Manufacturer (name) values ('Samsung');
 insert into Manufacturer (name) values ('LG');
 insert into Manufacturer (name) values ('GE');
 
--- insert into Freezer (FK_Freezer_email__HouseHold_email) values ('ktran322@gatech.edu', 'model blah blah', 'Samsung', 'side-by-side');
-insert into Freezer (FK_Freezer_email__HouseHold_email, name, model_name, model_type) values ('ktran322@gatech.edu', 'GE', 'French door', 'chest');
+-- insert into Freezer (FK_Freezer_email_HouseHold_email) values ('ktran322@gatech.edu', 'model blah blah', 'Samsung', 'side-by-side');
+insert into Freezer (FK_Freezer_email_HouseHold_email, name, model_name, model_type) values ('ktran322@gatech.edu', 'GE', 'French door', 'chest');
 
 SELECT * FROM PhoneNumber;
 
@@ -30,36 +30,36 @@ SELECT * FROM PhoneNumber;
 SELECT * FROM Freezer; 
 
 -- Force second applince for Khang
-insert into Freezer (freezer_id, FK_Freezer_email__HouseHold_email, name, model_name, model_type) values (0,'ktran322@gatech.edu', 'GE', 'neverworks', 'chest');
+insert into Freezer (freezer_id, FK_Freezer_email_HouseHold_email, name, model_name, model_type) values (0,'ktran322@gatech.edu', 'GE', 'neverworks', 'chest');
 
 SELECT * FROM Freezer;
 
-insert into Washer (washer_id, FK_Washer_email__HouseHold_email, name, model_name, loading_type) values (0, 'ktran322@gatech.edu', 'GE', 'neverworks', 'top');
-insert into Washer (washer_id, FK_Washer_email__HouseHold_email, name, model_name, loading_type) values (0, 'ktran322@gatech.edu', 'GE', 'neverworks', 'front');
-insert into Washer (washer_id, FK_Washer_email__HouseHold_email, name, model_name, loading_type) values (0, 'ktran322@gatech.edu', 'GE', 'neverworks', 'top');
+insert into Washer (washer_id, FK_Washer_email_HouseHold_email, name, model_name, loading_type) values (0, 'ktran322@gatech.edu', 'GE', 'neverworks', 'top');
+insert into Washer (washer_id, FK_Washer_email_HouseHold_email, name, model_name, loading_type) values (0, 'ktran322@gatech.edu', 'GE', 'neverworks', 'front');
+insert into Washer (washer_id, FK_Washer_email_HouseHold_email, name, model_name, loading_type) values (0, 'ktran322@gatech.edu', 'GE', 'neverworks', 'top');
 SELECT * FROM Washer;
-insert into Dryer (Dryer_id, FK_Dryer_email__HouseHold_email, name, model_name, Heat_source) values (0, 'ktran322@gatech.edu', 'LG', 'alwaysbroken', 'gas');
-insert into Dryer (Dryer_id, FK_Dryer_email__HouseHold_email, name, model_name, Heat_source) values (0, 'ktran322@gatech.edu', 'GE', 'alwaysbroken', 'electric');
-insert into Dryer (Dryer_id, FK_Dryer_email__HouseHold_email, name, model_name, Heat_source) values (0, 'ktran322@gatech.edu', 'Samsung', 'alwaysbroken', 'none');
+insert into Dryer (Dryer_id, FK_Dryer_email_HouseHold_email, name, model_name, Heat_source) values (0, 'ktran322@gatech.edu', 'LG', 'alwaysbroken', 'gas');
+insert into Dryer (Dryer_id, FK_Dryer_email_HouseHold_email, name, model_name, Heat_source) values (0, 'ktran322@gatech.edu', 'GE', 'alwaysbroken', 'electric');
+insert into Dryer (Dryer_id, FK_Dryer_email_HouseHold_email, name, model_name, Heat_source) values (0, 'ktran322@gatech.edu', 'Samsung', 'alwaysbroken', 'none');
 SELECT * FROM Dryer;
 
-insert into TV (tv_id, FK_tv_email__HouseHold_email, name, model_name, Display_type, Display_size, Maximum_resolution) values (0, 'ktran322@gatech.edu', 'LG', 'alwaysbroken', 'LCD', 5, '126x126');
-insert into TV (tv_id, FK_tv_email__HouseHold_email, name, model_name, Display_type, Display_size, Maximum_resolution) values (0, 'ktran322@gatech.edu', 'GE', 'alwaysbroken', 'LCD', 0.9, '1000x1000');
-insert into TV (tv_id, FK_tv_email__HouseHold_email, name, model_name, Display_type, Display_size, Maximum_resolution) values (0, 'ktran322@gatech.edu', 'Samsung', 'alwaysbroken', 'LCD', 0.11111111111111, '4k');
+insert into TV (tv_id, FK_tv_email_HouseHold_email, name, model_name, Display_type, Display_size, Maximum_resolution) values (0, 'ktran322@gatech.edu', 'LG', 'alwaysbroken', 'LCD', 5, '126x126');
+insert into TV (tv_id, FK_tv_email_HouseHold_email, name, model_name, Display_type, Display_size, Maximum_resolution) values (0, 'ktran322@gatech.edu', 'GE', 'alwaysbroken', 'LCD', 0.9, '1000x1000');
+insert into TV (tv_id, FK_tv_email_HouseHold_email, name, model_name, Display_type, Display_size, Maximum_resolution) values (0, 'ktran322@gatech.edu', 'Samsung', 'alwaysbroken', 'LCD', 0.11111111111111, '4k');
 SELECT * FROM TV;
 
 
-insert into Cooker (Cooker_id, FK_Cooker_email__HouseHold_email, name, model_name) values (0, 'ktran322@gatech.edu', 'LG', 'alwaysbroken');
-insert into Cooker (Cooker_id, FK_Cooker_email__HouseHold_email, name, model_name) values (0, 'ktran322@gatech.edu', 'GE', 'alwaysbroken');
-insert into Cooker (Cooker_id, FK_Cooker_email__HouseHold_email, name, model_name) values (0, 'ktran322@gatech.edu', 'Samsung', 'alwaysbroken');
+insert into Cooker (Cooker_id, FK_Cooker_email_HouseHold_email, name, model_name) values (0, 'ktran322@gatech.edu', 'LG', 'alwaysbroken');
+insert into Cooker (Cooker_id, FK_Cooker_email_HouseHold_email, name, model_name) values (0, 'ktran322@gatech.edu', 'GE', 'alwaysbroken');
+insert into Cooker (Cooker_id, FK_Cooker_email_HouseHold_email, name, model_name) values (0, 'ktran322@gatech.edu', 'Samsung', 'alwaysbroken');
 SELECT * FROM Cooker;
 
-insert into Oven (FK_Oven_id_Cooker_cooker_id, FK_Oven_email__HouseHold_email, oven_type, has_gas_heat_source, has_electric_heat_source, has_microwave_heat_source) values (3, 'ktran322@gatech.edu', 'conventional', TRUE, FALSE, TRUE);
-insert into Oven (FK_Oven_id_Cooker_cooker_id, FK_Oven_email__HouseHold_email, oven_type, has_gas_heat_source, has_electric_heat_source, has_microwave_heat_source) values (1, 'ktran322@gatech.edu', 'conventional', FALSE, FALSE, FALSE);
--- insert into Oven (FK_Oven_id_Cooker_cooker_id, FK_Oven_email__HouseHold_email, oven_type, has_gas_heat_source, has_electric_heat_source, has_microwave_heat_source) values (0, 'ktran322@gatech.edu', 'conventional', TRUE, TRUE, TRUE);
+insert into Oven (FK_Oven_id_Cooker_cooker_id, FK_Oven_email_HouseHold_email, oven_type, has_gas_heat_source, has_electric_heat_source, has_microwave_heat_source) values (3, 'ktran322@gatech.edu', 'conventional', TRUE, FALSE, TRUE);
+insert into Oven (FK_Oven_id_Cooker_cooker_id, FK_Oven_email_HouseHold_email, oven_type, has_gas_heat_source, has_electric_heat_source, has_microwave_heat_source) values (1, 'ktran322@gatech.edu', 'conventional', FALSE, FALSE, FALSE);
+-- insert into Oven (FK_Oven_id_Cooker_cooker_id, FK_Oven_email_HouseHold_email, oven_type, has_gas_heat_source, has_electric_heat_source, has_microwave_heat_source) values (0, 'ktran322@gatech.edu', 'conventional', TRUE, TRUE, TRUE);
 SELECT * FROM Oven;
 
-insert into Cooktop (FK_Cooktop_id_Cooker_cooker_id, FK_Cooktop_email__HouseHold_email, Heat_source) values (1, 'ktran322@gatech.edu', 'gas');
-insert into Cooktop (FK_Cooktop_id_Cooker_cooker_id, FK_Cooktop_email__HouseHold_email, Heat_source) values (2, 'ktran322@gatech.edu', 'electric');
-insert into Cooktop (FK_Cooktop_id_Cooker_cooker_id, FK_Cooktop_email__HouseHold_email, Heat_source) values (3, 'ktran322@gatech.edu', 'none');
+insert into Cooktop (FK_Cooktop_id_Cooker_cooker_id, FK_Cooktop_email_HouseHold_email, Heat_source) values (1, 'ktran322@gatech.edu', 'gas');
+insert into Cooktop (FK_Cooktop_id_Cooker_cooker_id, FK_Cooktop_email_HouseHold_email, Heat_source) values (2, 'ktran322@gatech.edu', 'electric');
+insert into Cooktop (FK_Cooktop_id_Cooker_cooker_id, FK_Cooktop_email_HouseHold_email, Heat_source) values (3, 'ktran322@gatech.edu', 'none');
 SELECT * FROM Cooktop;
