043bfbb3cc59535e273109cb9796d78b05c2ceaf
Khang Tran <khangtran@Khangs-MacBook-Pro.local>
2022-11-11 21:29:52 -0800

    complete email verification, and add postal code component (incomplete)

--- Phase_3/backend/app.py
+++ Phase_3/backend/app.py
@@ -45,6 +45,37 @@ api.add_resource(Household, '/household/<email>')
 
 
 
+class PostalCode(Resource):
+    def get(self, postal_code:str):
+        '''
+        api request example: 
+        curl --location --request GET '127.0.0.1:5000/postal_code/08889'
+
+        query the PostalCode table for the postal information
+        return: Code 200: {'postal_code': <postal_cpde>, 'city':<city>, 'state':<state>}
+                Code 404: postal code not found
+                Code 500: internal server error
+        '''       
+        try:
+            db.cursor.execute('''SELECT postal_code, city, state 
+            FROM PostalCode WHERE
+            PostalCode.postal_code = %s''', (postal_code, ))
+            res = db.cursor.fetchall()
+            print(res)
+            if res:
+                resDict = {'postal_code': res[0][0],
+                            'city': res[0][1],
+                            'state': res[0][2]}
+                return({'result': resDict}, 200)
+            return({'result': 'postal code not found'}, 404)
+        except Exception as e:
+            return(f'Server side error: {e}', 500)
+api.add_resource(PostalCode, '/postal_code/<postal_code>')
+
+
+
+
+
 if __name__ == '__main__':
     try:
         app.run(debug = True)
--- Phase_3/client/src/App.js
+++ Phase_3/client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import {EmailForm} from "./components/emailform"
+import {EmailForm} from "./components/household/emailform"
+import { PostalCodeForm } from './components/household/postalform';
+import {NavBarComponent} from "./components/common/navbar"
 
 /* The following line can be included in your src/index.js or App.js file */
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,7 +10,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+      <NavBarComponent/>
       <EmailForm/>
+      <PostalCodeForm/>
     </div>
   );
 }
